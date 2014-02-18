5500c1e2095cd494f0089b8b4ce29053f6093713
brendan <brendan.heberton@gmail.com>
2014-02-18 15:09:43 -0700

    events working

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,18 +1,9 @@
 document.addEventListener('WebComponentsReady', function() {
   document.querySelector('stylist-element').addEventListener('color-changed', function(e) {
-    $('woot-map').changeFill( e.detail.msg );
-    //map.getLayer(map.graphicsLayerIds[0]).renderer.symbol.color = new dojo.Color(  );
-    //map.getLayer(map.graphicsLayerIds[0]).redraw();
+    document.querySelector('woot-map').changeFill( e.detail.msg );
   });
 
   document.querySelector('stylist-element').addEventListener('size-changed', function(e) {
-    $('woot-map').changeSize( e.detail.msg );
-    //map.getLayer(map.graphicsLayerIds[0]).renderer.symbol.size = e.detail.msg;
-    //map.getLayer(map.graphicsLayerIds[0]).redraw();
-  });
-
-  document.querySelector('stylist-element').addEventListener('style-changed', function(e) {
-    //var style = e.detail.msg;
-    //document.getElementById("style-output").innerHTML = JSON.stringify(style, null, 4);
+    document.querySelector('woot-map').changeSize( e.detail.msg );
   });
 });
