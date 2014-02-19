4b9d9dd677449d1d46fa2e2fbb227bdaa0d264fc
mjuniper <mjuniper@gmail.com>
2014-02-18 17:55:56 -0800

    killing debugger

--- app/elements/javascripts/woot-details.js
+++ app/elements/javascripts/woot-details.js
@@ -4,7 +4,6 @@ Polymer('woot-details', {
   update: function (which, attributes) {
     this.trail = which === 'trail';
     this.vrbo = which === 'vrbo';
-    debugger;
     for (var p in attributes) {
       if (attributes.hasOwnProperty(p)) {
         this[p] = attributes[p];
