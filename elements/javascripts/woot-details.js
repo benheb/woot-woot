de11c6fbcf76f6c9deedd932cf36d7bedfae2429
mjuniper <mjuniper@gmail.com>
2014-02-18 19:31:04 -0800

    Updates

--- elements/javascripts/woot-details.js
+++ elements/javascripts/woot-details.js
@@ -1,13 +1,31 @@
 'use strict';
 
 Polymer('woot-details', {
-  update: function (which, attributes) {
-    this.trail = which === 'trail';
-    this.vrbo = which === 'vrbo';
-    for (var p in attributes) {
-      if (attributes.hasOwnProperty(p)) {
-        this[p] = attributes[p];
+  vrbo: false,
+  trail: false,
+  home: true,
+  updateAttributes: function (attributes) {
+    if (attributes) {
+      for (var p in attributes) {
+        if (attributes.hasOwnProperty(p)) {
+          this[p] = attributes[p];
+        }
       }
+    }
+    this.home = false;
+    this.trail = false;
+    this.vrbo = false;
+    this.vrbos = false;
+  },
+  updateVrbo: function (attributes) {    
+    this.updateAttributes(attributes);
+    this.vrbo = true;
+  },
+  updateTrail: function (attributes, vrbos) {
+    this.updateAttributes(attributes);
+    this.trail = true;
+    if (vrbos) {
+      this.vrbos = vrbos;
     }
   }
 });
