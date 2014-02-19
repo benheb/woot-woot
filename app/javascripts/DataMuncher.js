97cd51aecee1e54aa54d089fe0921534c1f1540a
Dave Bouwman <dave@davebouwman.com>
2014-02-18 17:22:00 -0800

    datamuncher and bower

---
+++ app/javascripts/DataMuncher.js
@@ -0,0 +1,26 @@
+var DataMuncher = {
+  
+  /**
+   * Aggregate an array of Graphics into chartable data
+   * - parse price
+   */
+  aggregateProperties: function(graphics){
+    var graphable = {};
+
+    graphics.forEach(function(g){
+
+    });
+
+    return graphable;
+  },
+
+  /**
+   * parse the price values from 
+   * @param  {[type]} priceString [description]
+   * @return {[type]}             [description]
+   */
+  _parsePrice: function(priceString){
+
+  }
+
+};
