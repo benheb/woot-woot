0f6a6c64d8fe72e4d6bfef51d3cf480d0d1132ad
mjuniper <mjuniper@gmail.com>
2014-02-18 18:56:07 -0800

    conditional template

--- app/elements/javascripts/woot-details.js
+++ app/elements/javascripts/woot-details.js
@@ -1,7 +1,11 @@
 'use strict';
 
 Polymer('woot-details', {
+  vrbo: false,
+  trail: false,
+  home: true,
   update: function (which, attributes) {
+    this.home = false;
     this.trail = which === 'trail';
     this.vrbo = which === 'vrbo';
     for (var p in attributes) {
