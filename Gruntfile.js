05df7d2c87e900919efe1c105137135947b07688
ajturner <aturner@esri.com>
2014-02-18 19:05:21 -0800

    Another gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -245,7 +245,7 @@ module.exports = function (grunt) {
             }
           }     
     });
-    grunt.loadNpmTasks('grunt-build-gh-pages');
+    grunt.loadNpmTasks('grunt-gh-pages');
 
     grunt.registerTask('server', function (target) {
         grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
@@ -295,10 +295,5 @@ module.exports = function (grunt) {
         'jshint',
         // 'test'
         'build'
-    ]);
-    grunt.registerTask("deploy", [
-        'clean',
-        'copy',
-        'build_gh_pages:gh_pages'
     ]);
 };
